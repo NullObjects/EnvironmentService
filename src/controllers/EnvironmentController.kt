24e1ec00ffd1fa71842ba6b1f4b94cbc2b41e8bb
NullObjects <2982927629qq@gmail.com>
2020-10-14 10:16:57 +0800

    add environment/time location

--- src/controllers/EnvironmentController.kt
+++ src/controllers/EnvironmentController.kt
@@ -13,7 +13,6 @@ import org.ktorm.entity.first
 import org.ktorm.entity.forEach
 import org.ktorm.entity.sortedByDescending
 import java.time.LocalDateTime
-import java.time.LocalTime
 
 /**
  * environment路由
@@ -24,8 +23,9 @@ class EnvironmentController() {
     @Location("/{span}")
     data class Span(val env: EnvironmentController, val span: String = "last")
 
-    @Location("/{start}&&{end}")
-    data class Time(val env: EnvironmentController, val start: LocalTime, val end: LocalTime)
+    //TODO: 接收参数LocalDateTime出现问题，计划修改为接收string类型参数
+    @Location("/{start}/{end}")
+    data class Time(val env: EnvironmentController, val start: LocalDateTime, val end: LocalDateTime)
 }
 
 /**
@@ -34,54 +34,78 @@ class EnvironmentController() {
 @KtorExperimentalLocationsAPI
 fun Route.environment() {
     /**
+     * 获取最新一条数据
+     */
+    fun getData(database: Database) =
+        try {
+            val env = database.Environment.sortedByDescending { it.id }.first()
+            mapOf(
+                "id" to env.id,
+                "temperature" to env.temperature,
+                "humidity" to env.humidity,
+                "recordTime" to env.recordTime.toString()
+            )
+        } catch (ex: Exception) {
+            mapOf("getData failed" to ex.message)
+        }
+
+    /**
+     * 获取一段时间内数据
+     */
+    fun getData(database: Database, startTime: LocalDateTime, endTime: LocalDateTime) =
+        try {
+            val envList = mutableListOf<Map<String, Any>>()
+            database.Environment.filter { it.recordTime.between(startTime..endTime) }
+                .forEach {
+                    envList.add(
+                        mapOf(
+                            "id" to it.id,
+                            "temperature" to it.temperature,
+                            "humidity" to it.humidity,
+                            "recordTime" to it.recordTime.toString()
+                        )
+                    )
+                }
+            envList
+        } catch (ex: Exception) {
+            mutableListOf(mapOf("getData failed" to ex.message))
+        }
+
+    /**
      * 按时间段获取数据
      */
     get<EnvironmentController.Span> {
-        var startTime = LocalDateTime.now()
         val endTime = LocalDateTime.now()
         //根据时间段获取起始时间
-        startTime = when (it.span) {
+        val startTime = when (it.span) {
             "hour" -> endTime.minusHours(1)
             "day" -> endTime.minusDays(1)
             "week" -> endTime.minusDays(7)
             "month" -> endTime.minusMonths(1)
             else -> endTime
         }
-        val table = DruidUtil.getDataSource()?.let { source -> Database.connect(source).Environment }
-        if (startTime != endTime) {
-            //起止时间不同，返回一段时间内的数据
-            table?.let { tab ->
-                val envList = mutableListOf<Map<String, Any>>()
-                tab.filter { items -> items.recordTime.between(startTime..endTime) }
-                    .forEach { item ->
-                        envList.add(
-                            mapOf(
-                                "id" to item.id,
-                                "temperature" to item.temperature,
-                                "humidity" to item.humidity,
-                                "recordTime" to item.recordTime.toString()
-                            )
-                        )
-                    }
-                call.respond(envList)
-            } ?: call.respondText("get data failed")
-        } else {
-            //起止时间相同，返回最新一条数据
-            table?.let { tab ->
-                val env = tab.sortedByDescending { items -> items.id }.first()
-                call.respond(
-                    mapOf(
-                        "id" to env.id,
-                        "temperature" to env.temperature,
-                        "humidity" to env.humidity,
-                        "recordTime" to env.recordTime.toString()
-                    )
-                )
-            } ?: call.respondText("get data failed")
-        }
 
-//    get<Environment.Time> {
-//
-//    }
+        val database = DruidUtil.getDataSource()?.let { source -> Database.connect(source) }
+        if (database == null)
+            call.respondText("connect failed")
+        else {
+            //根据时间选择获取数据
+            if (startTime == endTime)
+                call.respond(getData(database))
+            else
+                call.respond(getData(database, startTime, endTime))
+        }
+    }
+
+    /**
+     * 按起止时间获取数据
+     */
+    get<EnvironmentController.Time> {
+        //TODO:路由接收参数由LocalDateTime转为String后，需添加it.start和it.end的转换
+        val database = DruidUtil.getDataSource()?.let { source -> Database.connect(source) }
+        if (database == null)
+            call.respondText("connect failed")
+        else
+            call.respond(getData(database, it.start, it.end))
     }
 }
